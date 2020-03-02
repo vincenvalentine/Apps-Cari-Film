0cd2d2018caf31ad458d1bd22da98b4ba0af7427
vincenvalentine <genz18keyz@gmail.com>
2020-03-02 21:11:03 +0700

    Update Design

--- javascript.js
+++ javascript.js
@@ -1,65 +1,81 @@
-function searchMovie() {
-    let btnSearch = document.getElementById('search');
-    let inputSearch = document.getElementById('input-search');
-    let content = document.getElementById('content');
+let btnSearch = document.getElementById('search');
+let inputSearch = document.getElementById('input-search');
+let content = document.getElementById('content');
+let contentFilm = document.getElementById('content-film');
 
-    btnSearch.addEventListener('click', function () {
-        $.ajax({
-            url: 'http://www.omdbapi.com',
-            type: 'get',
-            dataType: 'json',
-            data: {
-                'apikey': '47bf85bb',
-                's': inputSearch.value,
-            },
-            success: function (result) {
-                if (result.Response == "True") {
-                    let movies = result.Search;
-                    let contentFilm = document.getElementById('content-film');
+function MovieList() {
+    $.ajax({
+        url: 'http://www.omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': '47bf85bb',
+            's': inputSearch.value,
+        },
+        success: function (result) {
 
-                    let filmList = [];
+            if (result.Response == "True") {
+                let movies = result.Search;
 
-                    console.log(movies.length);
-                    
+                let filmList = [];
 
-                    for (let i = 0; i < movies.length; i++) {
-                        filmList.push(`
-                            <div class="content-film" >
-                                    <div class="content-film-title">
-                                        <p>${movies[i].Title}</p>
-                                    </div>
-                                    <div class="content-film-img">
-                                        <img src="${movies[i].Poster}" alt="">
-                                    </div>
-                                    <div class="content-film-btn">
-                                        <p>See Detail >></p>
-                                    </div>
-                            </div>
-                        `)
-                    }
+                console.log(movies.length);
 
-                    join = filmList.join('');
-                    contentFilm.innerHTML = join;
-                    console.log(filmList);
-                    
-                    console.log(movies);
-                    
-                    console.log(movies[1].Title);
-                    
+                console.log(filmList);
 
-
-                } else {
-                    content.innerHTML = `<h2 class="error">${result.Error}</h2>`;
-
+                for (let i = 0; i < movies.length; i++) {
+                    filmList.push(`
+                        <div class="content-film" >
+                                <div class="content-film-img">
+                                    <img src="${movies[i].Poster}" alt="">
+                                </div>
+                                <div class="content-film-title">
+                                    <h4>${movies[i].Title}</h4>
+                                    <p>${movies[i].Year}</p>
+                                </div>
+                                <div class="content-film-btn">
+                                    <p>See Detail >></p>
+                                </div>
+                        </div>
+                    `)
                 }
-                // console.log(result);
+
+                join = filmList.join('');
+                contentFilm.innerHTML = join;
+
+                console.log(join);
+
+
+            } else {
+                content.innerHTML = `<h2 class="error">${result.Error}</h2>`;
 
             }
-        });
-    })
+            console.log(result);
 
-
-
+        }
+    });
 }
 
+function searchMovie() {
+
+    btnSearch.addEventListener('click', function () {
+        
+        MovieList();
+        // window.location.reload();
+    })
+
+    inputSearch.onkeyup = function(e){
+        if (e.which === 13) {
+            MovieList();
+        }
+        
+    }
+}
+
+
+
+
+
+
 searchMovie();
+
